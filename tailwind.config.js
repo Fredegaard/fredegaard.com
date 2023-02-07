e3fef9dd459a35a9f6f700d09e4a43e5a756fda4
Christian Fredegaard <fredegaard@hey.com>
2023-02-07 23:58:29 +0100

    Added AlpineJS and implemented DarkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ["./*.{html,js}"],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(rgba(0, 0, 0, 0) 1.5px, #fff 1px)',
-      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
